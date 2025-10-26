4aa617dd87164378c25ce117d58350167797f6e6
Vinh2805 <vinhv4818@gmail.com>
2025-10-26 22:53:49 +0700

    a

---
+++ Momentia/Laravel/storage/framework/views/021a7de3c0ab7f3d149933ddb71a6a32.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/logo/Logo_Momentia.png" type="image/png">
+    <title>Momentia</title>
+    <?php echo app('Illuminate\Foundation\Vite')->reactRefresh(); ?>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/src/components/main/main.tsx'); ?>
+</head>
+<body>
+    <div id="root"></div>
+</body>
+</html><?php /**PATH D:\PHP\photo\Momentia\Laravel\resources\views/main.blade.php ENDPATH**/ ?>
