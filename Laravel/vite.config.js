d8b8b77575f02bfcad6c40841e8f67e284475652
Kien <kien24134@gmail.com>
2026-01-04 19:22:58 +0700

    complete

--- Laravel/vite.config.js
+++ Laravel/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
             input: [
                 "resources/css/app.css",
                 "resources/js/app.js",
-                "resources/src/components/main/main.tsx"
+                "resources/src/main.tsx"
             ],
             refresh: true,
         }),
